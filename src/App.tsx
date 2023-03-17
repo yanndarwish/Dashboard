a706ef5b13fcd4551abb88dbe3f925bb2896e735
Yann Darwish <yann.darwish@gmail.com>
2023-03-17 15:04:55 +0100

    custom theme setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-function App() {
+import { ColorModeContext, useMode } from "./theme"
+import { CssBaseline, ThemeProvider } from "@mui/material"
 
-  return (
-    <div className="app">
-    </div>
-  )
+function App() {
+	const [theme, colorMode] = useMode()
+
+	return (
+		<ColorModeContext.Provider value={colorMode}>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				<div className="app">
+					<main className="content"></main>
+				</div>
+			</ThemeProvider>
+		</ColorModeContext.Provider>
+	)
 }
 
 export default App
